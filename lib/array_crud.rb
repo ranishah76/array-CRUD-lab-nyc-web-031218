1d3437a03805abbb869f8c7e508863156234a46e
Rani Shah <rani76@gmail.com>
2018-02-26 20:51:47 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element 
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.first 
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array.last 
+  array.last
 end
